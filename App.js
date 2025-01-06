66580f9e5c865f0705f3e02d7b5f5caa897bb786
Ahmed Sami <ahmedsamihamid1@gmail.com>
2025-01-06 22:36:26 +0400

    add navigations to the app

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
-import tw from "twrnc";
+// import tw from "twrnc";
+import AppNavigation from "./navigation";
 
 // nativewind approach
 export default function App() {
-  return (
-    <View className="flex-1 justify-center items-center bg-white ">
-      <TouchableOpacity className="bg-teal-800 p-3 rounded-lg shadow-md shadow-gray-400">
-        <Text className="text-white text-3xl font-bold ">Hello World</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="dark" />
-    </View>
-  );
+  return <AppNavigation />;
 }
 
 // // twrnc approach
